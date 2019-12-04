2a769528fc6a140de6cfb22f36abcac68f87d841
JiaqiJin96 <fdsam@hotmail.com>
2019-12-04 23:01:37 +0100

    action move

---
+++ TFGDS/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    //Variables para entrada de teclados
+    public string keyUp;
+    public string keyDown;
+    public string keyLeft;
+    public string keyRight;
+
+
+    public float Dup;
+    public float Dright;
+
+    public bool inputEnable = true; // varibale para comprobar si puede recibir señal o no
+
+    // variables para smoothdamp
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+       
+        //Entrada de la coordenado x and y 
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+
+        if (!inputEnable)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+
+        //Gradually changes a value towards a desired goal over time.
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+    }
+}
